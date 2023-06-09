cb361718d3d685daf0a982e48a46de198405c0f7
Qa5im <24100256@lums.edu.pk>
2023-06-10 00:23:04 +0500

    fixed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
-
-
-
-
-const button_el=document.getElementById("icon")
-const menu_el= document.getElementById("menu")
-const menu_list_el= document.getElementById("menu_list")
-const nav_items_el= document.getElementById("nav_items")
-console.log(menu_el)
-console.log(button_el)
-button_el.addEventListener("click",()=>{
-    if(menu_el.style.display === "block"){
-        menu_el.style.display = "none"
-    }
-    else{
-        menu_el.style.display="block"
-    }
-
-})
-menu_list_el.addEventListener("click",()=>{
-    menu_el.style.display="none"
-})
+const button_el = document.getElementById("icon");
+const menu_el = document.getElementById("menu");
+const menu_list_el = document.getElementById("menu_list");
+const nav_items_el = document.getElementById("nav_items");
+console.log(menu_el);
+console.log(button_el);
+button_el.addEventListener("click", () => {
+  if (menu_el.style.display === "block") {
+    menu_el.style.display = "none";
+  } else {
+    menu_el.style.display = "block";
+  }
+});
+menu_list_el.addEventListener("click", () => {
+  menu_el.style.display = "none";
+});
